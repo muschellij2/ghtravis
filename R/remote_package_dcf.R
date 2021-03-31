48c73259af9aed538f69730c830e47566326acaa
muschellij2 <muschellij2@gmail.com>
2021-03-31 12:35:11 -0400

    output_directory for downloading the descriptions

--- R/remote_package_dcf.R
+++ R/remote_package_dcf.R
@@ -4,19 +4,25 @@
 #' @param pat GitHub Personal Authentication Token (PAT)
 #' @param url URL for repo - github
 #' @param ... additional arguments to pass to \code{\link{GET}}
+#' @param output_directory Output directory for the description file to download to,
+#' @param overwrite overwrite the file if exists.  Only really relevant if
+#' \code{output_directory} is not \code{NULL}
 #'
 #' @return List of Package/Version DCF information
 #' @export
 #'
 #' @examples
 #' get_remote_package_dcf("stnava/ANTsRCore")
+#' out = get_remote_package_dcf("stnava/ANTsRCore", output_directory = tempdir())
+#' out = get_remote_package_dcf("stnava/ANTsRCore", output_directory = tempdir())
 #' remote_package_dcf("stnava/ANTsRCore")
-#' remote_package_dcf("stnava/ANTsRCore")
-
+#' remote_package_dcf("stnava/ANTsRCore", output_directory = tempdir())
 get_remote_package_dcf = function(
   remotes,
   pat = NULL,
   url = c("https://github.com", "https://api.github.com"),
+  output_directory = NULL,
+  overwrite = FALSE,
   ...) {
   if (is.null(pat)) {
     pat = devtools::github_pat(quiet = TRUE)
@@ -33,49 +39,59 @@ get_remote_package_dcf = function(
   }
   remote = parse_one_remote(remotes)
 
-  tmp <- tempfile()
-  api_call = grepl("api", tolower(url))
-  if (api_call) {
-    if (!requireNamespace("base64enc", quietly = TRUE)) {
-      stop("base64enc needed for API calls")
-    }
+  if (is.null(output_directory)) {
+    tmp <- tempfile()
+  } else {
+    dir.create(output_directory, showWarnings = FALSE, recursive = TRUE)
+    xxfile = paste(
+      remote$username, remote$repo,
+      remote$ref, remote$subdir, "DESCRIPTION", sep = "_")
+    tmp = file.path(output_directory, xxfile)
   }
-  if (!api_call) {
-    path <- paste(c(
-      remote$username,
-      remote$repo, "raw", remote$ref,
-      remote$subdir, "DESCRIPTION"),
-      collapse = "/")
-    ctype = httr::content_type("text/plain")
+  if (!file.exists(tmp) || overwrite) {
+    api_call = grepl("api", tolower(url))
+    if (api_call) {
+      if (!requireNamespace("base64enc", quietly = TRUE)) {
+        stop("base64enc needed for API calls")
+      }
+    }
+    if (!api_call) {
+      path <- paste(c(
+        remote$username,
+        remote$repo, "raw", remote$ref,
+        remote$subdir, "DESCRIPTION"),
+        collapse = "/")
+      ctype = httr::content_type("text/plain")
 
-  } else {
-    path <- paste(c(
-      "repos",
-      remote$username,
-      remote$repo, "contents",
-      remote$subdir, "DESCRIPTION"),
-      collapse = "/")
-    path = paste0(path, "?ref=", remote$ref)
-    ctype = httr::content_type_json()
-  }
-  req <- httr::GET(url, path = path,
-                   if (api_call) github_auth(pat),
-                   httr::write_disk(path = tmp),
-                   ctype,
-                   ...)
-  if (httr::status_code(req) >= 400) {
-    if (api_call) {
-      tmp = get_remote_package_dcf(remotes,
-                                   pat = pat, url = "https://github.com")
     } else {
-      tmp = NA_character_
+      path <- paste(c(
+        "repos",
+        remote$username,
+        remote$repo, "contents",
+        remote$subdir, "DESCRIPTION"),
+        collapse = "/")
+      path = paste0(path, "?ref=", remote$ref)
+      ctype = httr::content_type_json()
     }
-  } else {
-    if (api_call) {
-      data = httr::content(req)$content
-      data = base64enc::base64decode(what = data)
-      data = rawToChar(data)
-      writeLines(data, tmp)
+    req <- httr::GET(url, path = path,
+                     if (api_call) github_auth(pat),
+                     httr::write_disk(path = tmp),
+                     ctype,
+                     ...)
+    if (httr::status_code(req) >= 400) {
+      if (api_call) {
+        tmp = get_remote_package_dcf(remotes,
+                                     pat = pat, url = "https://github.com")
+      } else {
+        tmp = NA_character_
+      }
+    } else {
+      if (api_call) {
+        data = httr::content(req)$content
+        data = base64enc::base64decode(what = data)
+        data = rawToChar(data)
+        writeLines(data, tmp)
+      }
     }
   }
   if (is.na(tmp)) {
